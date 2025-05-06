8940ec0a77f8396fc5319127274e6cf565130d4f
vijayasimhareddy1 <vijayasimhareddy2003@gmail.com>
2025-05-06 09:31:54 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,10 +1,15 @@
 package com.test;
-
+import java.util.*;
 public class Program1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.println("enter the number");
+		int e=n*n;
+		System.out.println("the square of the number is "+e);
 	}
 
 }
