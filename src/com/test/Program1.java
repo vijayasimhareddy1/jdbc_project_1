76639392163549d8ee9840b8f45cc33091fd5da5
vijayasimhareddy1 <vijayasimhareddy2003@gmail.com>
2025-05-06 09:41:45 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -6,6 +6,7 @@ public class Program1 {
 		
 		System.out.println("hello world!!");
 		int n;
+		System.out.println("I am editing");
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("enter the number");
